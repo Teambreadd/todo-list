9dc14ca7ded9cbedbf037f233e9a6a3575be65ae
LucasC <cailu@scotscollege.school.nz>
2025-07-02 11:03:08 +1200

    Implement basic create button functionality

--- Todolist Project/app.js
+++ Todolist Project/app.js
@@ -1,2 +1,16 @@
 const createButton = document.querySelector("#create-button")
+const todoListContainer = document.querySelector("#todo-list-container")
+
+createButton.addEventListener("click", function() {
+    // console.log("clicked!") -- Debug
+    const numberOfTodoLists = todoListContainer.children.length;
+    if (numberOfTodoLists >= 4) {
+        alert("Can't have more than 4 todo lists!");
+        return;
+    }
+    const frame = document.createElement("div");
+    todoListContainer.append(frame);
+    todoListContainer.classList.add("container-with-frame");
+    frame.classList.add("todo-list-frame");
+})
 
