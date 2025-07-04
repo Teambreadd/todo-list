bb52490966411d8b750b222da40f9007ed96310a
LucasC <cailu@scotscollege.school.nz>
2025-07-04 16:58:11 +1200

    Implement to-do list button functionality & further CSS styling

--- Todolist Project/app.js
+++ Todolist Project/app.js
@@ -9,12 +9,45 @@ createButton.addEventListener("click", function() {
         return;
     }
 
-    const template = document.getElementById("todo-list-template");
-    const clone = template.content.cloneNode(true);
+    const listTemplate = document.getElementById("todo-list-template");
+    const listTemplateClone = listTemplate.content.cloneNode(true);
 
-    todoListContainer.append(clone);
     if (!todoListContainer.classList.contains("container-with-frame")) {
         todoListContainer.classList.add("container-with-frame");
     }
+
+    const background = listTemplateClone.querySelector(".todo-list-frame");
+    // console.log(background)
+
+    const deleteButton = background.querySelector(".container .delete-list-button");
+
+    deleteButton.addEventListener("click", function() {
+        deleteButton.parentElement.parentElement.remove();
+        const numberOfTodoLists = todoListContainer.children.length;
+        if (numberOfTodoLists <= 0) {
+            if (todoListContainer.classList.contains("container-with-frame")) {
+                todoListContainer.classList.remove("container-with-frame");
+            }
+        }
+    })
+
+    const addButton = background.querySelector(".container .add-task-button");
+
+    addButton.addEventListener("click", function() {
+        const taskTemplate = document.getElementById("task-template");
+        const taskTemplateClone = taskTemplate.content.cloneNode(true);
+
+        const tasksContainer = addButton.parentElement.parentElement.querySelector(".tasks-container");
+
+        const deleteTaskButton = taskTemplateClone.querySelector(".task-div .task-delete-button-container .delete-task-button")
+
+        deleteTaskButton.addEventListener("click", function() {
+            deleteTaskButton.parentElement.parentElement.remove()
+        })
+
+        tasksContainer.append(taskTemplateClone)
+    })
+
+    todoListContainer.append(listTemplateClone);
 })
 
